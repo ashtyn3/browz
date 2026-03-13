c61f701704cf419c47ed9160e74b40ab6ee99b72
ashtyn3 <ashtynmorel@gmail.com>
2026-03-13 10:08:08 -0400

    Initial Commit

---
+++ browz/browzApp.swift
@@ -0,0 +1,17 @@
+//
+//  browzApp.swift
+//  browz
+//
+//  Created by ashtyn morel-blake on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct browzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ browzTests/browzTests.swift
@@ -0,0 +1,17 @@
+//
+//  browzTests.swift
+//  browzTests
+//
+//  Created by ashtyn morel-blake on 3/13/26.
+//
+
+import Testing
+@testable import browz
+
+struct browzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
